bf5c6f59460765f7beaca83cf788463b7485ceb1
MarcoRhayden <Rhayden@arkansoftware.com>
2024-02-04 05:39:50 -0300

    chore: release 1.x.x

---
+++ src/main/index.tsx
@@ -0,0 +1,11 @@
+import '@/presentation/styles/global.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <></>
+  </React.StrictMode>,
+  document.getElementById('main'),
+);

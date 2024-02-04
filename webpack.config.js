44332815297041059a44679a12fd1b27527e26bf
MarcoRhayden <Rhayden@arkansoftware.com>
2024-02-04 05:23:34 -0300

    chore: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    alias: {
+      '@': path.join(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    static: './public',
+    historyApiFallback: true,
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
